43b302de8ddd68991c415dde22ca45f5df926e05
Andrei Serbu <andreiserbu1606@gmail.com>
2025-07-10 18:28:05 +0200

    Updated Docs, installed pythonping and imported it

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -1,7 +1,7 @@
 import csv
-from pythonping import ping
+from pythonping import ping 
 with open("devices.csv", newline="") as csvfile:
+    spamreader = csv.reader(csvfile, delimiter='|', quotechar=' ')
     reader = csv.DictReader(csvfile)
     for row in reader:
         print(row)
-
