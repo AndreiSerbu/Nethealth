6a5b4ca99e020716b187443dee4fa4afc1574e62
Andrei Serbu <andreiserbu1606@gmail.com>
2025-07-11 17:01:09 +0200

    Worked on Poller.py and further researched csv in python

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -1,7 +1,15 @@
 import csv
-from pythonping import ping 
+from pysnmp.hlapi.v3arch.asyncio import *
+from pythonping import ping
+
+i=0
+cycleCount = 0
 with open("devices.csv", newline="") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter='|', quotechar=' ')
+    spamreader = csv.reader(csvfile, dialect="excel", delimiter='|', quotechar=' ')
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
+        cycleCount += 1
+        print(" sop: "" ", row, "Number of cycles: ", cycleCount, "; and i: ", i)
+        ping(target="10.0.0.10", verbose=True)
+        
+# last thing tried : to print row["IP"]. for some reason it doesnt work.
