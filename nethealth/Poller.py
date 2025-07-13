4c5303edc1ebbb18baee2714512774c9dffc5e2f
Andrei Serbu <andreiserbu1606@gmail.com>
2025-07-13 22:51:58 +0200

    Finished Poller.py

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -1,15 +1,9 @@
 import csv
-from pysnmp.hlapi.v3arch.asyncio import *
 from pythonping import ping
 
-i=0
-cycleCount = 0
-with open("devices.csv", newline="") as csvfile:
-    spamreader = csv.reader(csvfile, dialect="excel", delimiter='|', quotechar=' ')
-    reader = csv.DictReader(csvfile)
-    for row in reader:
+with open("devices.csv", "r") as csvfile:
+    reader = csv.DictReader(csvfile, dialect="excel", delimiter=";", quotechar='"')
+    for line in reader:
         cycleCount += 1
-        print(" sop: "" ", row, "Number of cycles: ", cycleCount, "; and i: ", i)
-        ping(target="10.0.0.10", verbose=True)
-        
-# last thing tried : to print row["IP"]. for some reason it doesnt work.
+        print(" Value: "" ", line["IP"])
+        ping(target=line["IP"], verbose=True)
