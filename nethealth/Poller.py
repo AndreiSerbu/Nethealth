06f5ae5923a5c35fa45a6e1f4f65cad3bc30a569
Andrei Serbu <andreiserbu1606@gmail.com>
2025-07-15 22:09:48 +0200

    Working on functionality with results.db

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -1,9 +1,26 @@
 import csv
+import datetime
 from pythonping import ping
+import sqlite3
+
+ct = datetime.datetime.now()
 
 with open("devices.csv", "r") as csvfile:
     reader = csv.DictReader(csvfile, dialect="excel", delimiter=";", quotechar='"')
     for line in reader:
-        cycleCount += 1
         print(" Value: "" ", line["IP"])
-        ping(target=line["IP"], verbose=True)
+        #ping(target=line["IP"], verbose=True)
+
+
+
+connection = sqlite3.connect("results.db")
+dbCursor = connection.cursor()
+#dbCursor = dbCursor.execute("CREATE TABLE results(timestamp, host, latency_ms, up_down)")
+dbCursor.execute(
+    "INSERT INTO results VALUES (?, ?, ?, ?)",
+            (ct, "test", "test", "test")
+)
+connection.commit()
+res = dbCursor.execute("SELECT timestamp FROM results")
+res.fetchone()
+
