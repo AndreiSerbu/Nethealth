4cdb3216dac7de4bc38afc5f9ee1ca0fd961f3b4
Andrei Serbu <andreiserbu1606@gmail.com>
2025-07-18 22:02:30 +0200

    ct, host, latency dynamic in results.db

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -2,25 +2,22 @@ import csv
 import datetime
 from pythonping import ping
 import sqlite3
-
-ct = datetime.datetime.now()
-
+#dbCursor = dbCursor.execute("CREATE TABLE results(timestamp, host, latency_ms, up_down)")
+latency = 0
 with open("devices.csv", "r") as csvfile:
     reader = csv.DictReader(csvfile, dialect="excel", delimiter=";", quotechar='"')
     for line in reader:
-        print(" Value: "" ", line["IP"])
-        #ping(target=line["IP"], verbose=True)
-
-
-
-connection = sqlite3.connect("results.db")
-dbCursor = connection.cursor()
-#dbCursor = dbCursor.execute("CREATE TABLE results(timestamp, host, latency_ms, up_down)")
-dbCursor.execute(
-    "INSERT INTO results VALUES (?, ?, ?, ?)",
-            (ct, "test", "test", "test")
-)
-connection.commit()
-res = dbCursor.execute("SELECT timestamp FROM results")
-res.fetchone()
-
+        ct = datetime.datetime.now()
+        print("Time:", ct)
+        print("IP: ", line["IP"])
+        response = ping(target=line["IP"], count=4, verbose=True, out_format=None)
+        latency = response.rtt_avg_ms
+        host = line["Hostname"]
+        connection = sqlite3.connect("results.db")
+        dbCursor = connection.cursor()
+        dbCursor.execute(
+            "INSERT INTO results VALUES (?, ?, ?, ?)",
+                (ct, host, latency, "test")
+        )
+        connection.commit()
+        print("\n")
