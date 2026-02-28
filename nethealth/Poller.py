f11f1b4217427fe976ac9fe66f39f99d97fe8c42
Andrei Serbu <andreiserbu1606@gmail.com>
2026-02-28 22:20:57 +0100

    RAM monitoring added

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import time 
 from typing import List, Tuple, Optional
 from pythonping import ping
 from pysnmp.hlapi import(
@@ -14,6 +15,8 @@ hrProcessorLoad = "1.3.6.1.2.1.25.3.3.1.2"
 hrStorageType = "1.3.6.1.2.1.25.2.3.1.2"
 hrStorageRam = "1.3.6.1.2.1.25.2.1.2"
 hrStorageUsed = "1.3.6.1.2.1.25.2.3.1.6"
+hrStorageSize = "1.3.6.1.2.1.25.2.3.1.5"
+hrStorageAllocationUnits = "1.3.6.1.2.1.25.2.3.1.4"
 
 def snmp_get(ip: str, oid: str, port: int = 161, timeout: int = 3, retries: int = 2) -> str | None:
     iterator = getCmd(
@@ -44,7 +47,7 @@ def snmp_walk(ip: str, base_oid: str, port: int = 161, timeout: int = 3, retries
         UdpTransportTarget((ip, port), timeout=timeout, retries=retries),
         ContextData(),
         ObjectType(ObjectIdentity(base_oid)),
-        lexicographicMode=False,   # IMPORTANT: stop when leaving base_oid
+        lexicographicMode=False,   # stop when leaving base_oid
     )
 
     for (errorIndication, errorStatus, errorIndex, varBinds) in iterator:
@@ -55,14 +58,23 @@ def snmp_walk(ip: str, base_oid: str, port: int = 161, timeout: int = 3, retries
             print(f"SNMP WALK error on {ip}: {errorStatus.prettyPrint()} at {errorIndex}")
             return None
 
-        # varBinds is a list (usually length 1 here)
+
         for oid_obj, val_obj in varBinds:
             results.append((oid_obj.prettyPrint(), str(val_obj)))
 
     return results
 
 latency = 0
-with open("devices_simple.csv", "r") as csvfile:
+cpu_avg = 0
+
+#File path variables:
+resultsDB = r"C:\Projects\Network_Health\nethealth\results.db"
+csv_file = r"C:\Projects\Network_Health\nethealth\devices_simple.csv"
+
+#Main loop:
+main = 1
+
+with open(csv_file, "r") as csvfile:
     reader = csv.DictReader(csvfile, dialect="excel", delimiter=";", quotechar='"')
     for line in reader:
         ct = datetime.datetime.now()
@@ -87,18 +99,30 @@ with open("devices_simple.csv", "r") as csvfile:
                 cpu_avg = sum(loads) / len(loads)
             print("CPU avg:", cpu_avg, "%")
         
+
         rows = snmp_walk(line["IP"], hrStorageType)    
         loads = []
         if rows:
             for row in rows:
-                if row[1] == "1.3.6.1.2.1.25.2.1.2":
-                    idx = row[0].split(".")[-1]
-                    print(row)
-                    print(idx)
+                if row[1] == hrStorageRam:
+                    ramIndex = row[0].split(".")[-1]
+        
+        allocationUnits = snmp_get(line["IP"], oid = hrStorageAllocationUnits + "." + ramIndex)
 
+        ramSize = snmp_get(line["IP"], oid = hrStorageSize + "." + ramIndex)
+        ramSize = int(ramSize) * int(allocationUnits)
+        ramSizeMB = round(ramSize / (1024 ** 2), 2)
+        ramSizeGB = round(ramSize / (1024 ** 3), 2)
+        print("RAM Size: ", ramSizeMB, "MB" ," or" ,ramSizeGB ,"GB")
 
+        ramUsage = snmp_get(line["IP"], oid = hrStorageUsed + "." + ramIndex)
+        ramUsage = int(ramUsage) * int(allocationUnits)
+        ramUsageMB = round(ramUsage / (1024 ** 2), 2)
+        ramUsageGB = round(ramUsage / (1024 ** 3), 2)
+        print("RAM Usage: ", ramUsageMB, "MB" ," or" ,ramUsageGB ,"GB")
 
-        connection = sqlite3.connect("results.db")
+        #Add data to Database:
+        connection = sqlite3.connect(resultsDB)
         dbCursor = connection.cursor()
         dbCursor.execute(
             "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
@@ -106,6 +130,9 @@ with open("devices_simple.csv", "r") as csvfile:
         )  
         connection.commit()
         print("\n")
+        print("Waiting...")
+        time.sleep(2.5)
+        
 
 #SNMP OIDs:
 #Hostname (SYSNAME_OID): 1.3.6.1.2.1.1.5.0
