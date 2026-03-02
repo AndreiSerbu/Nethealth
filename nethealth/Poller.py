4120e4ff53a109a381522be925c82515bb020295
Andrei Serbu <andreiserbu1606@gmail.com>
2026-03-02 21:57:46 +0100

    Added Hard Disk monitoring

--- nethealth/Poller.py
+++ nethealth/Poller.py
@@ -17,6 +17,8 @@ hrStorageRam = "1.3.6.1.2.1.25.2.1.2"
 hrStorageUsed = "1.3.6.1.2.1.25.2.3.1.6"
 hrStorageSize = "1.3.6.1.2.1.25.2.3.1.5"
 hrStorageAllocationUnits = "1.3.6.1.2.1.25.2.3.1.4"
+hrStorageDescr = "1.3.6.1.2.1.25.2.3.1.3"
+hrStorageFixedDisk = "1.3.6.1.2.1.25.2.1.4"
 
 def snmp_get(ip: str, oid: str, port: int = 161, timeout: int = 3, retries: int = 2) -> str | None:
     iterator = getCmd(
@@ -66,6 +68,8 @@ def snmp_walk(ip: str, base_oid: str, port: int = 161, timeout: int = 3, retries
 
 latency = 0
 cpu_avg = 0
+fixedDiskTotalGB = 0
+fixedDiskTotalUsedGB = 0
 
 #File path variables:
 resultsDB = r"C:\Projects\Network_Health\nethealth\results.db"
@@ -77,7 +81,7 @@ main = 1
 with open(csv_file, "r") as csvfile:
     reader = csv.DictReader(csvfile, dialect="excel", delimiter=";", quotechar='"')
     for line in reader:
-        ct = datetime.datetime.now()
+        ct = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print("Time:", ct)
         print("IP: ", line["IP"])
         response = ping(target=line["IP"], count=4, verbose=True, out_format=None)
@@ -109,18 +113,18 @@ with open(csv_file, "r") as csvfile:
                     ramIndex = row[0].split(".")[-1]
         
         #Getting allocation units value to calculate RAM values into bytes
-        allocationUnits = snmp_get(line["IP"], oid = hrStorageAllocationUnits + "." + ramIndex)
+        ramAllocationUnits = snmp_get(line["IP"], oid = hrStorageAllocationUnits + "." + ramIndex)
 
         #Getting total RAM
         ramSize = snmp_get(line["IP"], oid = hrStorageSize + "." + ramIndex)
-        ramSize = int(ramSize) * int(allocationUnits)
+        ramSize = int(ramSize) * int(ramAllocationUnits)
         ramSizeMB = round(ramSize / (1024 ** 2), 2)
         ramSizeGB = round(ramSize / (1024 ** 3), 2)
         print("RAM Size: ", ramSizeMB, "MB" ," or" ,ramSizeGB ,"GB")
 
         #Getting total used RAM
         ramUsage = snmp_get(line["IP"], oid = hrStorageUsed + "." + ramIndex)
-        ramUsage = int(ramUsage) * int(allocationUnits)
+        ramUsage = int(ramUsage) * int(ramAllocationUnits)
         ramUsageMB = round(ramUsage / (1024 ** 2), 2)
         ramUsageGB = round(ramUsage / (1024 ** 3), 2)
         print("RAM Usage: ", ramUsageMB, "MB" ," or" ,ramUsageGB ,"GB")
@@ -130,14 +134,51 @@ with open(csv_file, "r") as csvfile:
         ramUsagePercent = round(ramUsagePercent)
         print(ramUsagePercent, "%")
 
+        #hrStorageFixedDisk
+        rows = snmp_walk(line["IP"], hrStorageType)    
+        if rows:
+            for row in rows:
+                if row[1] == hrStorageFixedDisk:
+                    diskIndex = row[0].split(".")[-1]
+                    
+                    fixedDiskAllocationUnits = snmp_get(line["IP"], oid = hrStorageAllocationUnits + "." + diskIndex)
+                    
+                    storageDescr = snmp_get(line["IP"], oid = hrStorageDescr + "." + diskIndex) 
+                    print(storageDescr)
+
+                    #Getting total size of each disk
+                    fixedDiskSize = snmp_get(line["IP"], oid = hrStorageSize + "." + diskIndex)
+                    fixedDiskSize = int(fixedDiskSize) * int(fixedDiskAllocationUnits)
+                    fixedDiskMB = round(fixedDiskSize / (1024 ** 2), 2)
+                    fixedDiskGB = round(fixedDiskSize / (1024 ** 3), 2)
+                    print("Disk size: ", fixedDiskMB, "MB" ," or" , fixedDiskGB,"GB")
+
+                    #Getting used size of each disk
+                    fixedDiskUsage = snmp_get(line["IP"], oid = hrStorageUsed + "." + diskIndex)
+                    fixedDiskUsage = int(fixedDiskUsage) * int(fixedDiskAllocationUnits)
+                    fixedDiskUsageMB = round(fixedDiskUsage / (1024 ** 2), 2)
+                    fixedDiskUsageGB = round(fixedDiskUsage / (1024 ** 3), 2)
+                    print("Disk used: ", fixedDiskUsageMB, "MB" ," or" , fixedDiskUsageGB,"GB")
+
+                    if fixedDiskGB > 1:
+                        fixedDiskTotalGB = fixedDiskTotalGB + fixedDiskGB
+                        print(fixedDiskTotalGB)
+                        fixedDiskTotalUsedGB = fixedDiskTotalUsedGB + fixedDiskUsageGB
+                        fixedDiskUsagePercent = fixedDiskUsageGB / fixedDiskGB * 100
+                        fixedDiskUsagePercent = round(fixedDiskUsagePercent, 2)
+                        print(fixedDiskUsagePercent)
+                    
+
         #Add data to DB:
         connection = sqlite3.connect(resultsDB)
         dbCursor = connection.cursor()
         dbCursor.execute(
-            "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?)",
-                (ct, host, latency, "test", cpu_avg, ramUsagePercent)
-        )  
+            "INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (ct, host, latency, "test", cpu_avg, ramUsagePercent, fixedDiskUsagePercent, fixedDiskTotalGB, fixedDiskTotalUsedGB)
+        )
         connection.commit()
         print("\n")
+
+
         print("Waiting...")
         time.sleep(2.5)
